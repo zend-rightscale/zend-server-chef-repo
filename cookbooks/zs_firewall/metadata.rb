76f0583aa8de3ce997b74f329ae19778aa83108e
amir@zend.com <amir@zend.com>
2012-04-01 10:13:17 +0000

    adding close ports recipe and renaming default recipe

--- cookbooks/zs_firewall/metadata.rb
+++ cookbooks/zs_firewall/metadata.rb
@@ -5,8 +5,9 @@ description      "Configuration of zs firewall"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
-recipe "zs_firewall::default","Configuration of Zend server firewall"
+recipe "zs_firewall::open_zs_ports","Configuration of Zend server firewall"
 recipe "zs_firewall::open_zscm_ports","Basic configuration of Zend Server Cluster Manager firewall"
+recipe "zs_firewall::close_zs_ports","closing the other Zend servers firewall for the server"
 depends "sys_firewall"
 depends "rs_utils"
 
