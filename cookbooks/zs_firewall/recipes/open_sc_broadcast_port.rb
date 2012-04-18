052145809048154bafb7847f5c8581be4ba9f2d6
amir@zend.com <amir@zend.com>
2012-04-18 07:38:23 +0000

    adding recipe to open broadcast port to work around udp port issue

---
+++ cookbooks/zs_firewall/recipes/open_sc_broadcast_port.rb
@@ -0,0 +1,19 @@
+#
+# Cookbook Name:: zs_firewall
+# Recipe:: default
+#
+# Copyright 2012, Zend Technologies 
+#
+# All rights reserved - Do Not Redistribute
+#
+log "Opening firewal ports for Zend Server"
+rs_utils_marker :begin
+    sys_firewall "Request all other appservers to open session clustering ports" do
+      machine_tag "loadbalancer:app=#{node[:lb][:applistener_name]}"
+      port 10070
+      protocol udp
+      enable true
+      ip_addr node[:cloud][:private_ips][0]
+      action :update_request
+    end
+rs_utils_marker :end
