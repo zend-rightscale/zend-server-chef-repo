24421751c6f08e01b564d86f95347c5d1624156a
amir@zend.com <amir@zend.com>
2012-04-23 07:03:00 +0000

    Fixing bug in zscm open recipe

--- cookbooks/zs_firewall/recipes/open_zscm_ports.rb
+++ cookbooks/zs_firewall/recipes/open_zscm_ports.rb
@@ -8,7 +8,7 @@
 #
 log "Opening firewal ports for Zend Server cluster manager"
 rs_utils_marker :begin
-if node[:sys_firewall_patched][:enabled] == "enabled"
+if node[:sys_firewall][:enabled] == "enabled"
   include_recipe "iptables"
   sys_firewall_patched "10081" # ZS gui HTTP
   sys_firewall_patched "10082" # ZS gui HTTPS
