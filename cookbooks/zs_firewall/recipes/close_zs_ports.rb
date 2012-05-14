23c70197591a28796b3cd7a5ba1332f0f9c30a45
amir@zend.com <amir@zend.com>
2012-05-14 08:01:07 +0000

    removing the sys_firewall patch

--- cookbooks/zs_firewall/recipes/close_zs_ports.rb
+++ cookbooks/zs_firewall/recipes/close_zs_ports.rb
@@ -12,7 +12,7 @@ if node[:sys_firewall][:enabled] == "enabled"
   include_recipe "iptables"
   app_server_rules = {10060=>'tcp', 10063=>'tcp', 10070 => 'udp'} #10060,10063,10070 are ports for Zend session clustering
   app_server_rules.each do |app_port,port_proto|
-    sys_firewall_patched "Request all other appservers to close session clustering ports" do
+    sys_firewall "Request all other appservers to close session clustering ports" do
       machine_tag "loadbalancer:app=#{node[:lb][:applistener_name]}"
       port app_port
       protocol port_proto
--- cookbooks/zs_firewall/recipes/open_zscm_ports.rb
+++ cookbooks/zs_firewall/recipes/open_zscm_ports.rb
@@ -10,7 +10,7 @@ log "Opening firewal ports for Zend Server cluster manager"
 rs_utils_marker :begin
 if node[:sys_firewall][:enabled] == "enabled"
   include_recipe "iptables"
-  sys_firewall_patched "10081" # ZS gui HTTP
-  sys_firewall_patched "10082" # ZS gui HTTPS
+  sys_firewall "10081" # ZS gui HTTP
+  sys_firewall "10082" # ZS gui HTTPS
 end
 rs_utils_marker :end
