e60ec4554e345423a9782edc280dbdaba49a37a2
amir@zend.com <amir@zend.com>
2012-05-15 18:49:55 +0000

    changing the tag it uses to open ports

--- cookbooks/zs_firewall/recipes/open_zs_ports.rb
+++ cookbooks/zs_firewall/recipes/open_zs_ports.rb
@@ -14,21 +14,21 @@ if node[:sys_firewall][:enabled] == "enabled"
   sys_firewall "10082" # ZS gui HTTPS
   app_server_rules = {10060=>'tcp',10063=>'tcp',10070=>'udp'} #10060,10063,10070 are ports for Zend session clustering
   app_server_rules.each do |app_port,port_proto|
-#    sys_firewall "Open this appserver to  port #{app_port} to all Zend Servers" do
- #     machine_tag "loadbalancer:app=#{node[:lb][:applistener_name]}"
-#      port app_port
-#      protocol port_proto
-#      enable true
-#      action :update
-#    end
-#    sys_firewall "Request all other appservers to open session clustering ports" do
-#      machine_tag "loadbalancer:app=#{node[:lb][:applistener_name]}"
-#      port app_port
-#      protocol port_proto
-#      enable true
-#      ip_addr node[:cloud][:private_ips][0]
-#      action :update_request
-#    end
+    sys_firewall "Open this appserver to  port #{app_port} to all Zend Servers" do
+      machine_tag "appserver:active=true"
+      port app_port
+      protocol port_proto
+      enable true
+      action :update
+    end
+    sys_firewall "Request all other appservers to open session clustering ports" do
+      machine_tag "appserver:active=true"
+      port app_port
+      protocol port_proto
+      enable true
+      ip_addr node[:cloud][:private_ips][0]
+      action :update_request
+    end
   end
 end
 rs_utils_marker :end
