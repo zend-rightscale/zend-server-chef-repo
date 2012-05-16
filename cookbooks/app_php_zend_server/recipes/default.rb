7a4405d9bd55e6c6c9d31852a07e2f216fc322a5
amir@zend.com <amir@zend.com>
2012-05-16 08:10:31 +0000

    Installing ZS infrastructure

--- cookbooks/app_php_zend_server/recipes/default.rb
+++ cookbooks/app_php_zend_server/recipes/default.rb
@@ -8,11 +8,39 @@
 rs_utils_marker :begin
 
 log " Setting provider specific settings for Zend server php application server."
-
+case node[:platform]
+when "ubuntu", "debian"
+  node[:zend][:server][:repofile] == "/etc/apt/sources.list.d/zend.list"
+when "centos","redhat"
+  node[:zend][:server][:repofile] == "/etc/yum.repos.d/zend.repo"
+else
+  raise "Unrecognized distro #{node[:platform]}, exiting "
+end
+node[:zend][:server][:version] == "5.6"
 node[:app][:provider] = "app_php_zend_server"
 node[:app][:destination]="#{node[:web_apache][:docroot]}"
-node[:app][:packages] = ["php5", "php5-mysql", "php-pear", "libapache2-mod-php5"]
-
+node[:zend][:server][:php_version]=5.3
+node[:app][:packages] = ["zend-server-php-" + node[:zend][:server][:php_version]]
+node[:zend][:server][:repo_baseurl]="http://repos.zend.com/zend-server"
+node[:zend][:server][:repo_suffix]="5.6"
+case node[:platform]
+when "ubuntu", "debian"
+  template "/etc/apt/sources.list.d/zend.list" do
+    source "zend.list.erb"
+  end
+execute "curl -s http://repos.zend.com/zend.key | apt-key add -" do
+  action :nothing
+end
+execute "apt-get update" do
+  action :nothing
+end
+when "centos", "redhat"
+  template "/etc/yum.repos.d/zend.repo" do
+    source "zend.repo.erb"
+  end
+else
+  raise "Unrecognized distro #{node[:platform]}, exiting "
+end
 rs_utils_marker :end
 
 
