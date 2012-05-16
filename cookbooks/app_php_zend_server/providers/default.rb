a1f34aabf6a0121dbefe80fea1ac0dc8204440f3
amir@zend.com <amir@zend.com>
2012-05-16 15:55:46 +0000

    bug fix

--- cookbooks/app_php_zend_server/providers/default.rb
+++ cookbooks/app_php_zend_server/providers/default.rb
@@ -34,12 +34,6 @@ end
 
 
   action :install do
-    execute "curl -s http://repos.zend.com/zend.key | apt-key add -" do
-    action :nothing
-  end
-  execute "apt-get update" do
-    action :nothing
-  end
   # Install user-specified Packages and Modules
   packages = new_resource.packages
   log "Packages which will be installed #{packages}"
