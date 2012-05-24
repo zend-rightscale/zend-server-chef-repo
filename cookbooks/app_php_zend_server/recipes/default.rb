6482bca630bd775f39f9d637d63b2b9e98d7663d
amir@zend.com <amir@zend.com>
2012-05-24 12:01:48 +0000

    using the right ip for firewall

--- cookbooks/app_php_zend_server/recipes/default.rb
+++ cookbooks/app_php_zend_server/recipes/default.rb
@@ -9,10 +9,10 @@ rs_utils_marker :begin
 
 log " Setting provider specific settings for Zend server php application server."
 node[:app][:packages] = Array.new
-if private_ips && private_ips.size > 0
-          ip = private_ips[0] # default to first private ip
-        elsif public_ips && public_ips.size > 0
-          ip = public_ips[0] # default to first public ip
+if node[:cloud][:private_ips] && node[:cloud][:private_ips].size > 0
+          ip = node[:cloud][:private_ips][0] # default to first private ip
+        elsif node[:cloud][:public_ips] && node[:cloud][:public_ips].size > 0
+          ip = node[:cloud][:public_ips][0] # default to first public ip
         elseif
           raise "ERROR: cannot detect a bind address on this application server."
         end
