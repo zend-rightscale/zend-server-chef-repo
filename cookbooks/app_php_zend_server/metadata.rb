0f14a70488d27af6ba0aaa710d12420fa94eb826
amir@zend.com <amir@zend.com>
2012-05-24 06:18:16 +0000

    cleaning garbage

--- cookbooks/app_php_zend_server/metadata.rb
+++ cookbooks/app_php_zend_server/metadata.rb
@@ -6,9 +6,9 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
 depends "app"
-depends "web_apache"
-depends "db_mysql"
-depends "repo"
-depends "rs_utils"
+#depends "web_apache"
+#depends "db_mysql"
+#depends "repo"
+#depends "rs_utils"
 
-recipe "app_php_zend_server::default", "Installs the Zend server php application server."
+recipe "app_php_zend_server::default", "remove php packages from packages to install"
--- cookbooks/app_php_zend_server/recipes/default.rb
+++ cookbooks/app_php_zend_server/recipes/default.rb
@@ -8,45 +8,8 @@
 rs_utils_marker :begin
 
 log " Setting provider specific settings for Zend server php application server."
-node[:app][:zend_server_version] = "5.6"
-node[:app][:zend_server_php_version]="5.3"
-#node[:app][:provider] = "app_php_zend_server"
-#node[:app][:destination]="#{node[:web_apache][:docroot]}"
-#node[:app][:packages] = ["zend-server-php-" + node[:app][:zend_server_php_version]]
 node[:app][:packages] = Array.new
-node[:app][:zend_server_repo_baseurl]="http://repos.zend.com/zend-server/"
-node[:app][:zend_server_repo_suffix]="5.6"
-node[:app][:zend_server_repo_url]=node[:app][:zend_server_repo_baseurl]+node[:app][:zend_server_repo_suffix]
-url=node[:app][:zend_server_repo_baseurl]+node[:app][:zend_server_repo_suffix] 
-case node[:platform]
-when "ubuntu", "debian"
-#  node[:app][:root]="/var/www"
-  template "/etc/apt/sources.list.d/zend.list" do
-    source "zend.list.erb"
-  end
-execute "curl -s http://repos.zend.com/zend.key | apt-key add -" do
-  action :nothing
-end
-execute "apt-get update" do
-  action :nothing
-end
-when "centos", "redhat"
-#node[:app][:root]="/var/www/html"
-#node[:app][:destination]="/var/www/html"
-#  yum_repository "zend" do
-#    description "Zend Server Repo"
-#    key node['repo']['zend']['http://repos.zend.com/zend.key']
-#    url node['repo']['zend']['http://repos.zend.com/zend-server/5.6']
-#    mirrorlist true
-#    action :add
-#    enabled 0
-#  end
-  template "/etc/yum.repos.d/zend.repo" do
-    source "zend.repo.erb"
-  end
-else
-  raise "Unrecognized distro #{node[:platform]}, exiting "
-end
+
 rs_utils_marker :end
 
 
