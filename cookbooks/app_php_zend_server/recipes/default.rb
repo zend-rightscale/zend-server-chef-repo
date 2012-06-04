49732ead9faa9758a4b6eca444f8fe8189bf8acb
amir@zend.com <amir@zend.com>
2012-06-04 12:01:41 +0000

    Fine tuning ports open and close actions

--- cookbooks/app_php_zend_server/recipes/default.rb
+++ cookbooks/app_php_zend_server/recipes/default.rb
@@ -8,19 +8,23 @@
 rs_utils_marker :begin
 
 log " Setting provider specific settings for Zend server php application server."
+# prevent app_php::default from installing the RS php
 node[:app][:packages] = Array.new
+# Remove RS mod php enable in Ubuntu apache
 case node[:platform]
 when "ubuntu", "debian"
   node[:php][:module_dependencies] = Array.new
   node[:php][:module_dependencies] = [ "proxy_http"]
 when "centos","fedora","redhat"
 end
+#Set the right IP for use in firewall rules
 if node[:cloud][:private_ips] && node[:cloud][:private_ips].size > 0
-          ip = node[:cloud][:private_ips][0] # default to first private ip
-        elsif node[:cloud][:public_ips] && node[:cloud][:public_ips].size > 0
-          ip = node[:cloud][:public_ips][0] # default to first public ip
-        elseif
-          raise "ERROR: cannot detect a bind address on this application server."
-        end
-node[:app][:bind_ip]=ip
+  ip = node[:cloud][:private_ips][0] # default to first private ip
+elsif node[:cloud][:public_ips] && node[:cloud][:public_ips].size > 0
+  ip = node[:cloud][:public_ips][0] # default to first public ip
+else
+  raise "ERROR: cannot detect a bind address on this application server."
+end
+node[:app][:bind_ip] = ip
+node[:app][:port] = 80
 rs_utils_marker :end
--- cookbooks/zs_firewall/recipes/open_zs_ports.rb
+++ cookbooks/zs_firewall/recipes/open_zs_ports.rb
@@ -10,8 +10,18 @@ log "Opening firewal ports for Zend Server"
 rs_utils_marker :begin
 if node[:sys_firewall][:enabled] == "enabled"
   include_recipe "iptables"
-  sys_firewall "10081" # ZS gui HTTP
-  sys_firewall "10082" # ZS gui HTTPS
+#  sys_firewall "10081" # ZS gui HTTP
+#  sys_firewall "10082" # ZS gui HTTPS
+  zscm_rules = {10081=>'tcp',10082=>'udp'} # gui ports for communicating with Zend server cluster manager
+  zscm_rules.each do |app_port,port_proto|
+    sys_firewall "Open this appserver to  port #{app_port} to Zend Server cluster manager" do
+      machine_tag "zscm:active=true "
+      port app_port
+      protocol port_proto
+      enable true
+      action :update
+    end
+  end
   app_server_rules = {10060=>'tcp',10063=>'tcp',10070=>'udp'} #10060,10063,10070 are ports for Zend session clustering
   app_server_rules.each do |app_port,port_proto|
     sys_firewall "Open this appserver to  port #{app_port} to all Zend Servers" do
@@ -29,6 +39,12 @@ if node[:sys_firewall][:enabled] == "enabled"
       ip_addr node[:app][:bind_ip] 
       action :update_request
     end
+  end
+  # close global port 80 so LB can later open it specifically to it
+  sys_firewall '80' do
+    protocol 'tcp'
+    enable false
+    action :update
   end
 end
 rs_utils_marker :end
