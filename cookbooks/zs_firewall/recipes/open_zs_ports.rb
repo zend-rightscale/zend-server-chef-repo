999b32da777ce4b1940dc5998a0081a95f1d01e4
amir@zend.com <amir@zend.com>
2012-06-05 09:34:51 +0000

    fine tuning

--- cookbooks/zs_firewall/recipes/open_zs_ports.rb
+++ cookbooks/zs_firewall/recipes/open_zs_ports.rb
@@ -10,18 +10,18 @@ log "Opening firewal ports for Zend Server"
 rs_utils_marker :begin
 if node[:sys_firewall][:enabled] == "enabled"
   include_recipe "iptables"
-#  sys_firewall "10081" # ZS gui HTTP
-#  sys_firewall "10082" # ZS gui HTTPS
-  zscm_rules = {10081=>'tcp',10082=>'udp'} # gui ports for communicating with Zend server cluster manager
-  zscm_rules.each do |app_port,port_proto|
-    sys_firewall "Open this appserver to  port #{app_port} to Zend Server cluster manager" do
-      machine_tag "appserver:zscm=true"
-      port app_port
-      protocol port_proto
-      enable true
-      action :update
-    end
-  end
+  sys_firewall "10081" # ZS gui HTTP
+  sys_firewall "10082" # ZS gui HTTPS
+#  zscm_rules = {10081=>'tcp',10082=>'udp'} # gui ports for communicating with Zend server cluster manager
+#  zscm_rules.each do |app_port,port_proto|
+#    sys_firewall "Open this appserver to  port #{app_port} to Zend Server cluster manager" do
+#      machine_tag "appserver:zscm=true"
+#      port app_port
+#      protocol port_proto
+#      enable true
+#      action :update
+#    end
+#  end
   app_server_rules = {10060=>'tcp',10063=>'tcp',10070=>'udp'} #10060,10063,10070 are ports for Zend session clustering
   app_server_rules.each do |app_port,port_proto|
     sys_firewall "Open this appserver to  port #{app_port} to all Zend Servers" do
