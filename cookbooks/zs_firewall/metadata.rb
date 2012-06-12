2b1210f5b93111f6a625b5bfb096a6e5e66b8573
amir@zend.com <amir@zend.com>
2012-06-12 06:50:28 +0000

    separate recipe for openning gui ports in single ZS

--- cookbooks/zs_firewall/metadata.rb
+++ cookbooks/zs_firewall/metadata.rb
@@ -5,7 +5,8 @@ description      "Configuration of zs firewall"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.5"
 
-recipe "zs_firewall::open_zs_ports","Configuration of Zend server firewall"
+recipe "zs_firewall::open_zs_ports","Configuration of clustered Zend server firewall"
+recipe "zs_firewall::open_zs_gui_ports","Configuration of Zend server firewall"
 recipe "zs_firewall::open_zscm_ports","Basic configuration of Zend Server Cluster Manager firewall"
 recipe "zs_firewall::close_zs_ports","closing the other Zend servers firewall for the server"
 depends "sys_firewall"
