cf702b6d56300d30916069c6bdd2792789114132
amir@zend.com <amir@zend.com>
2012-09-30 09:47:01 +0000

    updating metadata

--- cookbooks/app_php_zend_server/metadata.rb
+++ cookbooks/app_php_zend_server/metadata.rb
@@ -5,10 +5,6 @@ description      "Installs/Configures app_php_zend_server"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
-depends "app"
-#depends "web_apache"
-#depends "db_mysql"
-#depends "repo"
-#depends "rs_utils"
+depends "app_php"
 
-recipe "app_php_zend_server::default", "remove php packages from packages to install"
+recipe "app_php_zend_server::default", "set parameters for ZS install and add installation repositories"
