a369f66bf976d81dfb157c3a9d066a3050944651
amir@zend.com <amir@zend.com>
2012-10-03 07:12:14 +0000

    updating metadata

--- cookbooks/app_php_zend_server/metadata.rb
+++ cookbooks/app_php_zend_server/metadata.rb
@@ -4,5 +4,5 @@ license          "All rights reserved"
 description      "Configures app_php to install Zend Server"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
-depends "app_php"
+#depends "app_php"
 recipe "app_php_zend_server::default", "set parameters for ZS install and add installation repositories"
