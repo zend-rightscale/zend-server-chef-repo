b412d5a33018b949e94e9eefdfbe37e7c355a3f5
zend-rightscale <amir@zend.com>
2012-10-03 12:30:13 +0300

    Update cookbooks/app_php_zend_server/metadata.rb
    Add commented dependency on rightscale

--- cookbooks/app_php_zend_server/metadata.rb
+++ cookbooks/app_php_zend_server/metadata.rb
@@ -5,4 +5,5 @@ description      "Configures app_php to install Zend Server"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 #depends "app_php"
+#depends "rightscale"
 recipe "app_php_zend_server::default", "set parameters for ZS install and add installation repositories"
