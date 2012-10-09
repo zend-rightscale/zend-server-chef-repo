0eefb2872dd1398de8bb766f5f520f0c1ba39bc6
zend-rightscale <amir@zend.com>
2012-10-09 18:15:47 +0300

    Update cookbooks/app_php_zend_server/recipes/join_cluster.rb
    debugging mysql connection

--- cookbooks/app_php_zend_server/recipes/join_cluster.rb
+++ cookbooks/app_php_zend_server/recipes/join_cluster.rb
@@ -7,6 +7,7 @@ rightscale_marker :begin
 log "join local Zend server to cluster"
 code_to_run = <<-EOH
           web_api_key=`sqlite3 /usr/local/zend/var/db/gui.db "select HASH from GUI_WEBAPI_KEYS where NAME='zend-zsd';"`
+          mysql -h #{node[:app][:zend_server_mysql_address]} -u#{node[:app][:zend_server_mysql_user]} -p#{node[:app][:zend_server_mysql_password]} ZendServer
           /usr/local/zend/bin/zs-manage server-add-to-cluster -n #{node[:app][:zend_server_name]} -i #{node[:app][:zend_server_node_ip]}  -o #{node[:app][:zend_server_mysql_address]} -u #{node[:app][:zend_server_mysql_user]} -p #{node[:app][:zend_server_mysql_password]} -d ZendServer -N zend-zsd -K $web_api_key
           EOH
 log "The code to run is: #{code_to_run}"
