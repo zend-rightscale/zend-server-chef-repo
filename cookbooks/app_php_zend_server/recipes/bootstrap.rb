2cc08fe2676918161c298ea0d230c0ba19b09502
amir@zend.com <amir@zend.com>
2012-10-09 09:34:21 +0000

    bug fix

--- cookbooks/app_php_zend_server/recipes/bootstrap.rb
+++ cookbooks/app_php_zend_server/recipes/bootstrap.rb
@@ -10,7 +10,7 @@ bash "bootstrap_zs" do
       cwd "/tmp"
       code <<-EOH
           sed "s|^zend_sc.ha.use_broadcast\s*=.*|zend_sc.ha.use_broadcast=0|g" -i /usr/local/zend/etc/scd.ini
-          /usr/local/zend/bin/zs-manage bootstrap-single-server -p #{[node[:app][:zend_gui_password]} -o  #{[node[:app][:zend_order_number]} -l  #{[node[:app][:zend_license_key]} -a TRUE   -r TRUE
+          /usr/local/zend/bin/zs-manage bootstrap-single-server -p #{node[:app][:zend_gui_password]} -o  #{node[:app][:zend_order_number]} -l  #{node[:app][:zend_license_key]} -a TRUE   -r TRUE
           /usr/local/zend/bin/zendctl.sh restart
       EOH
     end
