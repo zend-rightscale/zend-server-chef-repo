3b1657f02b9a17a48421c9eb1fcd0e61e5135222
amir@zend.com <amir@zend.com>
2012-10-09 09:35:10 +0000

    bug fix

--- cookbooks/app_php_zend_server/recipes/join_cluster.rb
+++ cookbooks/app_php_zend_server/recipes/join_cluster.rb
@@ -10,7 +10,7 @@ bash "join_zs_to_cluster" do
       cwd "/tmp"
       code <<-EOH
           web_api_key=`sqlite3 /usr/local/zend/var/db/gui.db "select HASH from GUI_WEBAPI_KEYS where NAME='zend-zsd';"`
-          /usr/local/zend/bin/zs-manage server-add-to-cluster -n #{[node[:app][:zend_server_name]} -i #{[node[:app][:zend_server_node_ip]}  -o #{[node[:app][:zend_server_mysql_address]} -u #{[node[:app][:zend_server_mysql_user]} -p #{[node[:app][:zend_server_mysql_password]} -d ZendServer -N zend-zsd -K $web_api_key
+          /usr/local/zend/bin/zs-manage server-add-to-cluster -n #{node[:app][:zend_server_name]} -i #{node[:app][:zend_server_node_ip]}  -o #{node[:app][:zend_server_mysql_address]} -u #{node[:app][:zend_server_mysql_user]} -p #{node[:app][:zend_server_mysql_password]} -d ZendServer -N zend-zsd -K $web_api_key
       EOH
     end
 rightscale_marker :end
