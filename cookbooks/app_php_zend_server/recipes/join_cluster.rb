97e2549c126b389be6f90bc0e81d6bb4fd0547e9
zend-rightscale <amir@zend.com>
2012-10-09 17:17:35 +0300

    Update cookbooks/app_php_zend_server/recipes/join_cluster.rb
    Trying to debug failure

--- cookbooks/app_php_zend_server/recipes/join_cluster.rb
+++ cookbooks/app_php_zend_server/recipes/join_cluster.rb
@@ -5,12 +5,14 @@
 rightscale_marker :begin
 
 log "join local Zend server to cluster"
-bash "join_zs_to_cluster" do
-    user "root"
-      cwd "/tmp"
-      code <<-EOH
+code_to_run = <<-EOH
           web_api_key=`sqlite3 /usr/local/zend/var/db/gui.db "select HASH from GUI_WEBAPI_KEYS where NAME='zend-zsd';"`
           /usr/local/zend/bin/zs-manage server-add-to-cluster -n #{node[:app][:zend_server_name]} -i #{node[:app][:zend_server_node_ip]}  -o #{node[:app][:zend_server_mysql_address]} -u #{node[:app][:zend_server_mysql_user]} -p #{node[:app][:zend_server_mysql_password]} -d ZendServer -N zend-zsd -K $web_api_key
+log "The code to run is: #{code_to_run}"
+bash "join_zs_to_cluster" do
+      user "root"
+      cwd "/tmp"
+      code code_to_run
       EOH
     end
 rightscale_marker :end
