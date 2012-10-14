44dda020aec4590185c6b426040c5f39e2d5ad5f
zend-rightscale <amir@zend.com>
2012-10-14 16:23:19 +0300

    Update cookbooks/app_php_zend_server/recipes/bootstrap.rb

--- cookbooks/app_php_zend_server/recipes/bootstrap.rb
+++ cookbooks/app_php_zend_server/recipes/bootstrap.rb
@@ -3,15 +3,15 @@
 #
 # Copyright  Zend technologies Inc. 
 rightscale_marker :begin
-
-log "bootstrap Zend server into a single server mode"
-bash "bootstrap_zs" do
-    user "root"
-      cwd "/tmp"
-      code <<-EOH
+code_to_run= <<-EOH
           sed "s|^zend_sc.ha.use_broadcast\s*=.*|zend_sc.ha.use_broadcast=0|g" -i /usr/local/zend/etc/scd.ini
           /usr/local/zend/bin/zs-manage bootstrap-single-server -p #{node[:app][:zend_gui_password]} -o  #{node[:app][:zend_order_number]} -l  #{node[:app][:zend_license_key]} -a TRUE   -r TRUE
           /usr/local/zend/bin/zendctl.sh restart
       EOH
+log "bootstrap Zend server into a single server mode with #{code_to_run}"
+bash "bootstrap_zs" do
+    user "root"
+      cwd "/tmp"
+      code code_to_run
     end
 rightscale_marker :end
