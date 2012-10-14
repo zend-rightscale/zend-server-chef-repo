4dcecdadadf48f7d76f10465581835e0766839a9
zend-rightscale <amir@zend.com>
2012-10-14 19:30:58 +0300

    Update cookbooks/app_php_zend_server/recipes/leave_cluster.rb
    Fixing the bash code again

--- cookbooks/app_php_zend_server/recipes/leave_cluster.rb
+++ cookbooks/app_php_zend_server/recipes/leave_cluster.rb
@@ -4,10 +4,10 @@
 # Copyright  Zend technologies Inc. 
 rightscale_marker :begin
 code_to_run = <<-EOH
-          web_api_key=`mysql --skip-column-names -h #{node[:app][:zend_server_mysql_address]} -u#{node[:app][:zend_server_mysql_user]} -p#{node[:app][:zend_server_mysql_password]} ZendServer << EOF
+          web_api_key2=`mysql --skip-column-names -h 10.198.89.155 -uadminuser -ppassword ZendServer << EOF
           select HASH from GUI_WEBAPI_KEYS where NAME="zend-zsd";
-          EOF 
-          `
+EOF
+`
           node_id="$(grep zend_server_daemon.cluster_node_id /usr/local/zend/etc/zsd.ini | cut -d= -f 2 | cut -d@ -f 1)"
           /usr/local/zend/bin/zs-manage cluster-remove-server -N zend-zsd -K $web_api_key $web_api_key
       EOH
