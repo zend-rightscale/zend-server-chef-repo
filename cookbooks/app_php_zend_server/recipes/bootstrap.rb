4f5ef7c0f3d324bf6623c199141cabc7e5adb6d7
zend-rightscale <amir@zend.com>
2012-10-15 10:43:25 +0300

    Update cookbooks/app_php_zend_server/recipes/bootstrap.rb
    Moving ZS installation here so it will be done after the vhost creation

--- cookbooks/app_php_zend_server/recipes/bootstrap.rb
+++ cookbooks/app_php_zend_server/recipes/bootstrap.rb
@@ -3,12 +3,18 @@
 #
 # Copyright  Zend technologies Inc. 
 rightscale_marker :begin
+
+log "Install and bootstrap Zend server into a single server mode"
+zs_package = "zend-server-php-" + node[:app][:zs_php_ver]
+package zs_package do
+  action :install
+end
 code_to_run= <<-EOH
           sed "s|^zend_sc.ha.use_broadcast\s*=.*|zend_sc.ha.use_broadcast=0|g" -i /usr/local/zend/etc/scd.ini
           /usr/local/zend/bin/zs-manage bootstrap-single-server -p #{node[:app][:zend_gui_password]} -o  #{node[:app][:zend_order_number]} -l  #{node[:app][:zend_license_key]} -a TRUE   -r TRUE
           /usr/local/zend/bin/zendctl.sh restart
       EOH
-log "bootstrap Zend server into a single server mode with #{code_to_run}"
+#log "Bootstrap code: #{code_to_run}"
 bash "bootstrap_zs" do
     user "root"
       cwd "/tmp"
