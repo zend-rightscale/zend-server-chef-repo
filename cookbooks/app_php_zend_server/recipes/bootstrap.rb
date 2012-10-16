ff3a290d7f303e025eaf3c14fa212aaa539cf2b0
zend-rightscale <amir@zend.com>
2012-10-16 15:41:45 +0300

    Update cookbooks/app_php_zend_server/recipes/bootstrap.rb
    ZSD to log level 3

--- cookbooks/app_php_zend_server/recipes/bootstrap.rb
+++ cookbooks/app_php_zend_server/recipes/bootstrap.rb
@@ -10,6 +10,7 @@ package zs_package do
   action :install
 end
 code_to_run= <<-EOH
+          sed "s|^zend_server_daemon.log_verbosity_level\s*=.*|zend_server_daemon.log_verbosity_level=3|g" -i /usr/local/zend/etc/zsd.ini
           sed "s|^zend_sc.ha.use_broadcast\s*=.*|zend_sc.ha.use_broadcast=0|g" -i /usr/local/zend/etc/scd.ini
           /usr/local/zend/bin/zs-manage bootstrap-single-server -p #{node[:app][:zend_gui_password]} -o  #{node[:app][:zend_order_number]} -l  #{node[:app][:zend_license_key]} -a TRUE   -r TRUE
           /usr/local/zend/bin/zendctl.sh restart
