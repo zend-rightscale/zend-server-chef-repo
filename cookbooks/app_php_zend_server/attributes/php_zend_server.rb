be9a383a20401b06be38c8fe7c70c361689b3de4
amir@zend.com <amir@zend.com>
2012-11-28 13:51:36 +0000

    bug fix

--- cookbooks/app_php_zend_server/attributes/php_zend_server.rb
+++ cookbooks/app_php_zend_server/attributes/php_zend_server.rb
@@ -7,7 +7,6 @@
 #
 default[:app_php_zend_server][:repo_key_url]='http://repos.zend.com/zend.key'
 default[:app][:port] = 80
-default[:app_php_zend_server][:package_name] = "zend-server-php-" + node[:app][:zs_php_ver]
 # Recommended attributes
 #
 
--- cookbooks/app_php_zend_server/recipes/install.rb
+++ cookbooks/app_php_zend_server/recipes/install.rb
@@ -4,6 +4,7 @@
 # Copyright  Zend technologies Inc. 
 rightscale_marker :begin
 log "install Zend Server"
+node[:app_php_zend_server][:package_name] = "zend-server-php-" + node[:app_php_zend_server][:php_ver]
 package node[:app_php_zend_server][:package_name] do
   action :install
 end
