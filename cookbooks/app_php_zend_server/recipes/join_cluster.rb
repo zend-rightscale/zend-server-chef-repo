bf7ffc46edb81ef7747106d92cc9a2531767a468
zend-rightscale <amir@zend.com>
2012-12-20 15:30:39 +0200

    Always exit 0 on join cluster

--- cookbooks/app_php_zend_server/recipes/join_cluster.rb
+++ cookbooks/app_php_zend_server/recipes/join_cluster.rb
@@ -6,9 +6,10 @@ rightscale_marker :begin
 
 log "join local Zend server to cluster"
 code_to_run = <<-EOH
-          set -e
+          #set -e
           web_api_key=`sqlite3 /usr/local/zend/var/db/gui.db "select HASH from GUI_WEBAPI_KEYS where NAME='zend-zsd';"`
           /usr/local/zend/bin/zs-manage server-add-to-cluster -n #{node[:app][:zend_server_name]} -i #{node[:app][:zend_server_node_ip]}  -o #{node[:app][:zend_server_mysql_address]} -u #{node[:app][:zend_server_mysql_user]} -p #{node[:app][:zend_server_mysql_password]} -d ZendServer -N zend-zsd -K $web_api_key
+          exit 0
           EOH
 log "The code to run is: #{code_to_run}"
 bash "join_zs_to_cluster" do
