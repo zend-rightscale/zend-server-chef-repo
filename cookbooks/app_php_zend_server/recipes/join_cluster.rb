ab202ceee8d471b754d2ed0cec1b4029c0713a92
zend-rightscale <amir@zend.com>
2012-12-20 14:35:30 +0200

    Exit on error

--- cookbooks/app_php_zend_server/recipes/join_cluster.rb
+++ cookbooks/app_php_zend_server/recipes/join_cluster.rb
@@ -6,15 +6,9 @@ rightscale_marker :begin
 
 log "join local Zend server to cluster"
 code_to_run = <<-EOH
+          set -e
           web_api_key=`sqlite3 /usr/local/zend/var/db/gui.db "select HASH from GUI_WEBAPI_KEYS where NAME='zend-zsd';"`
-          #if nc -zv -w120 $HOSTIP 22 <<< ” &> /dev/null
-          #then
-          #          log 'MySQL Port is open'
-                    /usr/local/zend/bin/zs-manage server-add-to-cluster -n #{node[:app][:zend_server_name]} -i #{node[:app][:zend_server_node_ip]}  -o #{node[:app][:zend_server_mysql_address]} -u #{node[:app][:zend_server_mysql_user]} -p #{node[:app][:zend_server_mysql_password]} -d ZendServer -N zend-zsd -K $web_api_key
-          #else
-          #          log 'MySQL Port is closed'
-          #          exit 1
-          #fi
+          /usr/local/zend/bin/zs-manage server-add-to-cluster -n #{node[:app][:zend_server_name]} -i #{node[:app][:zend_server_node_ip]}  -o #{node[:app][:zend_server_mysql_address]} -u #{node[:app][:zend_server_mysql_user]} -p #{node[:app][:zend_server_mysql_password]} -d ZendServer -N zend-zsd -K $web_api_key
           EOH
 log "The code to run is: #{code_to_run}"
 bash "join_zs_to_cluster" do
