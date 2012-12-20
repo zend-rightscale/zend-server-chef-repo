79458157b7a86a1c47c16ad4d407a7466aa158a6
zend-rightscale <amir@zend.com>
2012-12-20 14:29:02 +0200

    Wait for remove action to finish

--- cookbooks/app_php_zend_server/recipes/leave_cluster.rb
+++ cookbooks/app_php_zend_server/recipes/leave_cluster.rb
@@ -6,7 +6,7 @@ rightscale_marker :begin
 code_to_run = <<-EOH
           web_api_key=`echo "select HASH from GUI_WEBAPI_KEYS where NAME='zend-zsd';" | mysql --skip-column-names -h #{node[:app][:zend_server_mysql_address]} -u#{node[:app][:zend_server_mysql_user]} -p#{node[:app][:zend_server_mysql_password]} ZendServer`
           node_id="$(grep zend_server_daemon.cluster_node_id /usr/local/zend/etc/zsd.ini | cut -d= -f 2 | cut -d@ -f 1)"
-          /usr/local/zend/bin/zs-manage cluster-remove-server -N zend-zsd -K $web_api_key $node_id
+          /usr/local/zend/bin/zs-manage cluster-remove-server -N zend-zsd -K $web_api_key -s $node_id
       EOH
 log "detaching Zend server from the cluster with command: #{code_to_run}"
 bash "leave_cluster" do
