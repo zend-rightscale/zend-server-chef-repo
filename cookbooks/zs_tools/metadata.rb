ebadf44e03f53c743bd29d372975338b160c48df
amir@zend.com <amir@zend.com>
2013-02-08 18:49:20 +0000

    remove unused files/dirs

--- cookbooks/zs_tools/metadata.rb
+++
@@ -1,9 +0,0 @@
-maintainer       "Amir Friedman"
-maintainer_email "amir@zend.com"
-license          "All rights reserved"
-description      "Configuration of zs firewall"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.5"
-recipe "zs_tools::application_deploy","deploy an application to ZS/ZSCM stored on a cloud storage service"
-depends "repo"
-depends "repo_ros"
