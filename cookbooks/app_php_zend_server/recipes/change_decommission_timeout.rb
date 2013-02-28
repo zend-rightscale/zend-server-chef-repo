da34defa0c133a3778292d54ca349085950eaf60
zend-rightscale <amir@zend.com>
2013-02-28 19:04:21 +0200

    Update change_decommission_timeout.rb
    Added option to not change the decommission timeout if value is 'default'

--- cookbooks/app_php_zend_server/recipes/change_decommission_timeout.rb
+++ cookbooks/app_php_zend_server/recipes/change_decommission_timeout.rb
@@ -3,18 +3,20 @@
 #
 # Copyright Zend technologies Inc.
 rightscale_marker :begin
-log "change rightlink decommission timeout to #{node[:app_php_zend_server][:decommission_timeout]}"
-template "/tmp/change_decommission.sh" do
-  source "change_decommission.erb"
-  variables({
-    :decommission_timeout => node[:app_php_zend_server][:decommission_timeout]
-  })
+unless  node[:app_php_zend_server][:decommission_timeout] == 'default' do
+  log "change rightlink decommission timeout to #{node[:app_php_zend_server][:decommission_timeout]}"
+  template "/tmp/change_decommission.sh" do
+    source "change_decommission.erb"
+    variables({
+      :decommission_timeout => node[:app_php_zend_server][:decommission_timeout]
+    })
+  end
+  bash "change_decommission_timeout" do
+      user "root"
+      cwd "/tmp"
+      code <<-EOH
+      at now + 2 minutes -f /tmp/change_decommission.sh
+      EOH
+  end
 end
-bash "change_decommission_timeout" do
-    user "root"
-    cwd "/tmp"
-    code <<-EOH
-    at now + 2 minutes -f /tmp/change_decommission.sh
-EOH
-    end
 rightscale_marker :end
