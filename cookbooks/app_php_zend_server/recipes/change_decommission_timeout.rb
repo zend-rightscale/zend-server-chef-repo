a29fea36a253b0d5b40faa4d5c3c135f4fa2ba18
amir@zend.com <amir@zend.com>
2013-02-28 20:09:25 +0000

    fixing the syntax

--- cookbooks/app_php_zend_server/recipes/change_decommission_timeout.rb
+++ cookbooks/app_php_zend_server/recipes/change_decommission_timeout.rb
@@ -3,9 +3,7 @@
 #
 # Copyright Zend technologies Inc.
 rightscale_marker :begin
-If  node[:app_php_zend_server][:decommission_timeout] == 'default'
-  log "Not changing the default decommission value"
-else
+unless node[:app_php_zend_server][:decommission_timeout] == "default"
   log "change rightlink decommission timeout to #{node[:app_php_zend_server][:decommission_timeout]}"
   template "/tmp/change_decommission.sh" do
     source "change_decommission.erb"
@@ -20,5 +18,8 @@ else
       at now + 2 minutes -f /tmp/change_decommission.sh
       EOH
   end
+else 
+ log "Not changing the default decommission value"
 end
+
 rightscale_marker :end
