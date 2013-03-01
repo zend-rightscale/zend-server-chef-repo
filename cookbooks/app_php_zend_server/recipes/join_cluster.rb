f4a67b3c66986e83edefdcce36af63e2ac0e6ebf
zend-rightscale <amir@zend.com>
2013-03-01 21:12:22 +0200

    Update join_cluster.rb
    Removing unused commented code

--- cookbooks/app_php_zend_server/recipes/join_cluster.rb
+++ cookbooks/app_php_zend_server/recipes/join_cluster.rb
@@ -10,18 +10,10 @@ code_to_run = <<-EOH
           /usr/local/zend/bin/zs-manage store-directive -d 'zend_sc.ha.use_broadcast' -v '0' -N 'zend-zsd' -K $web_api_key
           /usr/local/zend/bin/zs-manage server-add-to-cluster -n #{node[:app_php_zend_server][:zend_server_name]} -i #{node[:app_php_zend_server][:zend_server_node_ip]}  -o #{node[:app_php_zend_server][:mysql_address]} -u #{node[:app_php_zend_server][:mysql_user]} -p #{node[:app_php_zend_server][:zend_server_mysql_password]} -d ZendServer -r 60 -w 5 -s -N zend-zsd -K $web_api_key
           EOH
-log "The code to run is: #{code_to_run}"
+#log "The code to run is: #{code_to_run}"
 bash "join_zs_to_cluster" do
       user "root"
       cwd "/tmp"
       code code_to_run
     end
-#bash "change sc to cloud mode" do
-#      user "root"
-#      cwd "/tmp"
-#      code <<-EOH 
-#          web_api_key=`echo "select HASH from GUI_WEBAPI_KEYS where NAME='zend-zsd';" | mysql --skip-column-names -h #{node[:app_php_zend_server][:mysql_address]} -u#{node[:app_php_zend_server][:mysql_user]} -p#{node[:app_php_zend_server][:zend_server_mysql_password]} ZendServer`
-#          /usr/local/zend/bin/zs-manage store-directive -d 'zend_sc.ha.use_broadcast' -v '0' -N 'zend-zsd' -K $web_api_key
-#          EOH
-#end
 rightscale_marker :end
