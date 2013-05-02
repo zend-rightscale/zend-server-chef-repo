4cdf8f5dfed723673f19ed39745a57a12e38ba5e
zend-rightscale <amir@zend.com>
2013-05-02 16:58:59 +0300

    Update install.rb

--- cookbooks/app_php_zend_server/recipes/install.rb
+++ cookbooks/app_php_zend_server/recipes/install.rb
@@ -19,10 +19,5 @@ bash "bootstrap_zs" do
           /usr/local/zend/bin/zs-manage bootstrap-single-server -p #{node[:app_php_zend_server][:gui_password]} -o  #{node[:app_php_zend_server][:order_number]} -l  #{node[:app_php_zend_server][:zend_license_key]} -a TRUE   -r TRUE || { log "bootstrap failed"; exit 1 ; }
       EOH
 end
-# Let others know we are an appserver
-# See http://support.rightscale.com/12-Guides/Chef_Cookbooks_Developer_Guide/Chef_Resources#RightLinkTag for the "right_link_tag" resource.
-    right_link_tag "appserver:active=true"
-    right_link_tag "appserver:listen_ip=#{node[:app][:ip]}"
-    right_link_tag "appserver:listen_port=#{node[:app][:port]}"
 
 rightscale_marker :end
