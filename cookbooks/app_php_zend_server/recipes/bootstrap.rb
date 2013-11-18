3b9199c5bcaa79efd8891448a7320271eda67d6f
zend-rightscale <amir@zend.com>
2013-11-18 15:57:26 +0200

    Update bootstrap.rb
    Enable bootstrap without license

--- cookbooks/app_php_zend_server/recipes/bootstrap.rb
+++ cookbooks/app_php_zend_server/recipes/bootstrap.rb
@@ -5,16 +5,30 @@
 rightscale_marker :begin
 log "bootstrap Zend Server"
 log "bootstrap Zend server into a single server mode"
-bash "bootstrap_zs" do
+bash "pre_bootstrap_zs" do
     user "root"
     cwd "/tmp"
     code <<-EOH
           /usr/local/zend/bin/zendctl.sh restart 
           sleep 4
           /usr/local/zend/bin/zs-manage api-keys-add-key -n #{node[:app_php_zend_server][:api_key_name]} -s #{node[:app_php_zend_server][:api_key]} 
-          /usr/local/zend/bin/zs-manage bootstrap-single-server -p #{node[:app_php_zend_server][:gui_password]} -o  #{node[:app_php_zend_server][:order_number]} -l  #{node[:app_php_zend_server][:zend_license_key]} -a TRUE   -r TRUE || { log "bootstrap failed"; exit 1 ; }
+          echo 'zend_gui.extra = "cloud=RightScale&template_version=6.2.0"' > /usr/local/zend/gui/config/zs_ui.ini
       EOH
 end
+if node[:app_php_zend_server][:order_number].empty or node[:app_php_zend_server][:zend_license_key].empty then
+    bootstrap =<<-EOH
+    /usr/local/zend/bin/zs-manage bootstrap-single-server -p #{node[:app_php_zend_server][:gui_password]} -a TRUE   -r TRUE || { log "bootstrap failed"; exit 1 ; }
+    EOH
+else
+    bootstrap =<<-EOH
+    /usr/local/zend/bin/zs-manage bootstrap-single-server -p #{node[:app_php_zend_server][:gui_password]} -o  #{node[:app_php_zend_server][:order_number]} -l  #{node[:app_php_zend_server][:zend_license_key]} -a TRUE   -r TRUE || { log "bootstrap failed"; exit 1 ; }
+    EOH
+end
+bash "bootstrap_zs" do
+      user "root"
+      cwd "/tmp"
+      code bootstrap
+    end
 # Let others know we are an appserver
 # See http://support.rightscale.com/12-Guides/Chef_Cookbooks_Developer_Guide/Chef_Resources#RightLinkTag for the "right_link_tag" resource.
     right_link_tag "appserver:active=true"
