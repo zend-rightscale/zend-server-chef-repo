07f0923f40aa2bedf9ecc71070f254d86449e200
Amir Friedman <afroyd@gmail.com>
2014-08-04 13:39:57 +0300

    Update bootstrap.rb
    bug fix

--- cookbooks/app_php_zend_server/recipes/bootstrap.rb
+++ cookbooks/app_php_zend_server/recipes/bootstrap.rb
@@ -14,7 +14,7 @@ bash "pre_bootstrap_zs" do
           /usr/local/zend/bin/zs-client.sh apiKeysAddKey --name=#{node[:app_php_zend_server][:api_key_name]} --username="admin" --hash #{node[:app_php_zend_server][:api_key]} 
       EOH
 end
-bs_command = "/usr/local/zend/bin/zs-client.sh bootstrapSingleServer --adminPassword=#{node[:app_php_zend_server][:gui_password]} --production=true -acceptEula"
+bs_command = "/usr/local/zend/bin/zs-client.sh bootstrapSingleServer --adminPassword=#{node[:app_php_zend_server][:gui_password]} --production=true --acceptEula"
 bs_command << "--orderNumber=#{node[:app_php_zend_server][:order_number]} --licenseKey=#{node[:app_php_zend_server][:zend_license_key]}" unless node[:app_php_zend_server][:order_number].nil? || node[:app_php_zend_server][:order_number].empty? || node[:app_php_zend_server][:zend_license_key].nil? || node[:app_php_zend_server][:zend_license_key].empty?
 bash "bootstrap_zs" do
       user "root"
