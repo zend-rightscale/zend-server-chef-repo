6fccde0c689db2d3ccbc35ffb16bcb38e773a850
Amir Friedman <afroyd@yahoo.com>
2014-08-04 12:33:52 +0300

    updating the join and leave cluster recipes to use ZendServerSDK instead of zs-manage

--- cookbooks/app_php_zend_server/recipes/leave_cluster.rb
+++ cookbooks/app_php_zend_server/recipes/leave_cluster.rb
@@ -5,7 +5,8 @@
 rightscale_marker :begin
 code_to_run = <<-EOH
           node_id="$(grep zend_server_daemon.cluster_node_id /usr/local/zend/etc/zsd.ini | cut -d= -f 2 | cut -d@ -f 1)"
-          /usr/local/zend/bin/zs-manage cluster-remove-server -N #{node[:app_php_zend_server][:api_key_name]} -K #{node[:app_php_zend_server][:api_key]} -s $node_id
+#          /usr/local/zend/bin/zs-manage cluster-remove-server -N #{node[:app_php_zend_server][:api_key_name]} -K #{node[:app_php_zend_server][:api_key]} -s $node_id
+/usr/local/zend/bin/zs-client.sh clusterRemoveServer --zskey=#{node[:app_php_zend_server][:api_key_name]} --zssecret=#{node[:app_php_zend_server][:api_key]} --serverId=$node_id --wait
       EOH
 log "detaching Zend server from the cluster with command: #{code_to_run}"
 bash "leave_cluster" do
