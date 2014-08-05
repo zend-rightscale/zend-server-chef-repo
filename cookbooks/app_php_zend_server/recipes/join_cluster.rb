765e810cb4ce3b9c7a44d5657fc473e5c4bf9d9e
Amir Friedman <afroyd@gmail.com>
2014-08-05 17:41:47 +0300

    Update join_cluster.rb

--- cookbooks/app_php_zend_server/recipes/join_cluster.rb
+++ cookbooks/app_php_zend_server/recipes/join_cluster.rb
@@ -7,7 +7,7 @@ rightscale_marker :begin
 log "join local Zend server to cluster"
 code_to_run = <<-EOH
           #/usr/local/zend/bin/zs-manage store-directive -d 'zend_sc.ha.use_broadcast' -v '0' -N #{node[:app_php_zend_server][:api_key_name]} -K #{node[:app_php_zend_server][:api_key]}
-          /usr/local/zend/bin/zs-client.sh configurationStoreDirectives --zskey=#{node[:app_php_zend_server][:api_key_name]} --zssecret=#{node[:app_php_zend_server][:api_key]} --directives="zend_sc.ha.use_broadcast=0&zend_debugger.allow_hosts=''&zend_gui.extra='cloud=rightscale'"
+  #        /usr/local/zend/bin/zs-client.sh configurationStoreDirectives --zskey=#{node[:app_php_zend_server][:api_key_name]} --zssecret=#{node[:app_php_zend_server][:api_key]} --directives="zend_sc.ha.use_broadcast=0&zend_debugger.allow_hosts=''&zend_gui.extra='cloud=rightscale'"
           #/usr/local/zend/bin/zs-manage store-directive -d 'zend_debugger.allow_hosts' -v '' -N #{node[:app_php_zend_server][:api_key_name]} -K #{node[:app_php_zend_server][:api_key]}
           #/usr/local/zend/bin/zs-manage store-directive -d 'zend_gui.extra' -v 'cloud=rightscale' -N #{node[:app_php_zend_server][:api_key_name]} -K #{node[:app_php_zend_server][:api_key]}
 #          /usr/local/zend/bin/zs-manage server-add-to-cluster -n #{node[:app_php_zend_server][:zend_server_name]} -i #{node[:app_php_zend_server][:zend_server_node_ip]}  -o #{node[:app_php_zend_server][:mysql_address]} -u #{node[:app_php_zend_server][:mysql_user]} -p #{node[:app_php_zend_server][:zend_server_mysql_password]} -d #{node[:app_php_zend_server][:zend_server_mysql_db_name]} -r 60 -w 5 -s -N #{node[:app_php_zend_server][:api_key_name]} -K #{node[:app_php_zend_server][:api_key]}
