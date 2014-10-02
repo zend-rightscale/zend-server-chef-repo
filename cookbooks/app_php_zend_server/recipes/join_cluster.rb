9ddbbd7be01b41ae988647ac5c2bf6e6696dd2ea
Amir Friedman <afroyd@gmail.com>
2014-10-02 14:50:48 +0300

    Update join_cluster.rb

--- cookbooks/app_php_zend_server/recipes/join_cluster.rb
+++ cookbooks/app_php_zend_server/recipes/join_cluster.rb
@@ -6,13 +6,16 @@ rightscale_marker :begin
 
 log "join local Zend server to cluster"
 code_to_run = <<-EOH
-          #/usr/local/zend/bin/zs-manage store-directive -d 'zend_sc.ha.use_broadcast' -v '0' -N #{node[:app_php_zend_server][:api_key_name]} -K #{node[:app_php_zend_server][:api_key]}
-          /usr/local/zend/bin/zs-client.sh configurationStoreDirectives --zskey=#{node[:app_php_zend_server][:api_key_name]} --zssecret=#{node[:app_php_zend_server][:api_key]} --directives="zend_sc.ha.use_broadcast=0&zend_debugger.allow_hosts=''"
-          #&zend_gui.extra='cloud=rightscale'"
-          #/usr/local/zend/bin/zs-manage store-directive -d 'zend_debugger.allow_hosts' -v '' -N #{node[:app_php_zend_server][:api_key_name]} -K #{node[:app_php_zend_server][:api_key]}
-          #/usr/local/zend/bin/zs-manage store-directive -d 'zend_gui.extra' -v 'cloud=rightscale' -N #{node[:app_php_zend_server][:api_key_name]} -K #{node[:app_php_zend_server][:api_key]}
-          /usr/local/zend/bin/zs-client.sh serverAddToCluster --zskey=#{node[:app_php_zend_server][:api_key_name]} --zssecret=#{node[:app_php_zend_server][:api_key]} --serverName=#{node[:app_php_zend_server][:zend_server_name]} --nodeIp=#{node[:app_php_zend_server][:zend_server_node_ip]} --dbHost=#{node[:app_php_zend_server][:mysql_address]} --dbUsername=#{node[:app_php_zend_server][:mysql_user]} --dbPassword=#{node[:app_php_zend_server][:zend_server_mysql_password]} --dbName=#{node[:app_php_zend_server][:zend_server_mysql_db_name]} 
-          #--wait
+          set +e
+          n=0
+          until [ $n -ge 13 ]
+          do
+                    /usr/local/zend/bin/zs-client.sh configurationStoreDirectives --zskey=#{node[:app_php_zend_server][:api_key_name]} --zssecret=#{node[:app_php_zend_server][:api_key]} --directives="zend_sc.ha.use_broadcast=0&zend_debugger.allow_hosts=''"
+                    /usr/local/zend/bin/zs-client.sh serverAddToCluster --zskey=#{node[:app_php_zend_server][:api_key_name]} --zssecret=#{node[:app_php_zend_server][:api_key]} --serverName=#{node[:app_php_zend_server][:zend_server_name]} --nodeIp=#{node[:app_php_zend_server][:zend_server_node_ip]} --dbHost=#{node[:app_php_zend_server][:mysql_address]} --dbUsername=#{node[:app_php_zend_server][:mysql_user]} --dbPassword=#{node[:app_php_zend_server][:zend_server_mysql_password]} --dbName=#{node[:app_php_zend_server][:zend_server_mysql_db_name]} --wait
+                    [ $? -eq 0 ] && break
+                    n=$[$n+1]
+                    sleep 3
+          done
           EOH
 log "The join cluster code is: #{code_to_run}"
 bash "join_zs_to_cluster" do
